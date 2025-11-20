347371ccfd2614bb106d6b112a6a678f08abec7a
gruv0o <renaultremi51100@gmail.com>
2025-11-20 20:23:45 +0100

    components

---
+++ src/components/Button.ts
@@ -0,0 +1,8 @@
+import Phaser from 'phaser';
+
+export default class Button extends Phaser.GameObjects.Container {
+    private bg: Phaser.GameObjects.Rectangle;
+    private text: Phaser.GameObjects.Text;
+
+    
+}
