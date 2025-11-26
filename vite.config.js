2587267106759c39447876b0322d9e96f0ecc935
gruv0o <renaultremi51100@gmail.com>
2025-11-26 14:38:32 +0100

    ~ vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         port: 5173
     },
     preview: {
-        port: 5173,
+        port: 80,
         host: '0.0.0.0',
         allowedHosts: ['remlit.fr'],
     }
