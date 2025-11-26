66f22c25816a4237ab42424b566712c36d7d8650
gruv0o <renaultremi51100@gmail.com>
2025-11-26 14:15:27 +0100

    + adding domain name

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173
+    },
+    preview: {
+        port: 5173,
+        host: '0.0.0.0',
+        allowedHosts: ['remlit.fr'],
+    }
+    // Ajoute ici tes plugins ou options spécifiques
+    // Exemple pour Phaser (optionnel) :
+    // plugins: [/* tes plugins */]
+});
